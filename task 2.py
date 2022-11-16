07df0d57b1684cab91d64d1140465b4b2b9617b7
user <leto1527@yandex.ru>
2022-11-16 20:06:28 +0300

    .

--- task 2.py
+++ task 2.py
@@ -1,8 +1,13 @@
+def conv():
+    b = ''
+    while n > 0:
+        b = str(n % 4) + b
+        n = n // 4
 
-
+    return b
 list = ["в", "и", "н", "т"]
 
-count = str(bin(1019)[2:])
+count = conv(1019)[2:]
 
 answer = ''
 for i in range(len(count)):
